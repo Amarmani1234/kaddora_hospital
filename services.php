b4fb0fe0c9d529c732b3a2a2d4495de373437bb4
Puneet Kumar <106093393+PuneetKumar2000@users.noreply.github.com>
2025-08-13 16:54:36 +0530

    add paragraph in service page

--- services.php
+++ services.php
@@ -1,3 +1,4 @@
 <?php  include_once('forms/header.php');  ?>
 <h1>Super Speciality Hospital In Noida</h1>
+<p>Welcome to our Hospital, we have latet medical technolgy with love and care.</p>
 <?php include_once('footer.php');  ?>
