8517dc702048427ebb35533d68b1e4e3fbddf3c0
Amarmani1234 <amaruniworld@gmail.com>
2025-08-13 17:48:31 +0530

    Add header and services page

--- services.php
+++ services.php
@@ -1,7 +1,3 @@
-
-<?php  include_once('header.php');  ?>
-<h1>Super Specility Hospital In Noida</h1>
-
 <?php  include_once('forms/header.php');  ?>
 <h1>Super Speciality Hospital In Noida</h1>
 <h6>
